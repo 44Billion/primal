948ab4f649890e95ff0996b7e9796099df77bce7
Bojan Mojsilovic <bmojsilovic@itekako.com>
2026-03-16 13:38:42 +0100

    Update missing wallet dialog

--- src/components/MissingNWCModal/MissingNWCModal.tsx
+++ src/components/MissingNWCModal/MissingNWCModal.tsx
@@ -4,6 +4,7 @@ import { Component } from 'solid-js';
 import styles from './MissingNWCModal.module.scss';
 import { hookForDev } from '../../lib/devTools';
 import AdvancedSearchDialog from '../AdvancedSearch/AdvancedSearchDialog';
+import ButtonPrimary from '../Buttons/ButtonPrimary';
 
 const MissingNWCModal: Component<{
   id?: string,
@@ -19,13 +20,22 @@ const MissingNWCModal: Component<{
       setOpen={(isOpen: boolean) => !isOpen && props.onClose?.()}
       title={
       <div class={styles.title}>
-        Missing Wallet
+        No Wallet Connected
       </div>
       }
       triggerClass={styles.hidden}
     >
       <div id={props.id} class={styles.modal}>
-        No wallet connected. You can configure it on the <a onClick={() => props.onClose?.()} href="/settings/nwc">Connected Wallets settings page</a>
+        <div class={styles.message}>
+          In order to send zaps, you need to connect a bitcoin wallet. You can do so on the <a onClick={() => props.onClose?.()} href="/settings/nwc">Connected Wallets</a> settings page.
+        </div>
+        <div class={styles.actions}>
+          <ButtonPrimary
+            onClick={props.onClose}
+          >
+            Close
+          </ButtonPrimary>
+        </div>
       </div>
 
     </AdvancedSearchDialog>
